10ab34c46e8c9465b670bd364d5077c9b24894f3
Chloe Brown <chloemail@protonmail.com>
2021-05-11 15:01:26 +0100

    kinda fully functioning, but deadline; sooo here we are

--- src/components/art/Search.js
+++ src/components/art/Search.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Search() {
   return (
-    <div className="loading-message hero is-fullheight">
-      <h1>Search ArtMatch</h1>
-      <p>Coming soon – search for your dream ArtMatch!</p>
+    <div className="loading-message hero">
+      <div className="hero-body">
+        <div className="container">
+          <h1>Search ArtMatch</h1>
+          <p>Coming soon – search for your dream ArtMatch!</p>
+        </div>
+      </div>
     </div>
   )
 }
